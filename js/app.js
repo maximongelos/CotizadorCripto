767d7d3d68360429af06fda80d261710e4f88276
Maxi Mongelos <maximongelos@gmail.com>
2022-04-09 05:05:07 -0300

    validar formulario y msj de error

--- js/app.js
+++ js/app.js
@@ -1,15 +1,28 @@
 const criptomonedasSelect = document.querySelector('#criptomonedas');
+const monedaSelect = document.querySelector('#moneda');
+const formulario = document.querySelector('#formulario');
+const resultado = document.querySelector('#resultado');
+
+const objBusqueda = {
+	moneda: '',
+	criptomoneda: '',
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+	consultarCriptomonedas();
+
+	formulario.addEventListener('submit', submitFormulario);
+
+	criptomonedasSelect.addEventListener('change', leerValor);
+
+	monedaSelect.addEventListener('change', leerValor);
+});
 
 //*Crear un promise
 const obtenerCriptomonedas = (criptomonedas) =>
 	new Promise((resolve) => {
-		console.log(criptomonedas);
 		resolve(criptomonedas);
 	});
-
-document.addEventListener('DOMContentLoaded', () => {
-	consultarCriptomonedas();
-});
 
 function consultarCriptomonedas() {
 	const url =
@@ -31,4 +44,33 @@ function selectCriptomonedas(criptomonedas) {
 
 		criptomonedasSelect.appendChild(option);
 	});
+}
+
+function leerValor(e) {
+	objBusqueda[e.target.name] = e.target.value;
+}
+
+function submitFormulario(e) {
+	e.preventDefault();
+
+	//!Validar
+
+	const {moneda, criptomoneda} = objBusqueda;
+
+	if (moneda === '' || criptomoneda === '') {
+		mostrarAlerta('Ambos campos son obligatorios');
+		return;
+	}
+}
+
+function mostrarAlerta(mensaje) {
+	const divMensaje = document.createElement('div');
+	divMensaje.classList.add('error');
+	divMensaje.textContent = mensaje;
+
+	formulario.appendChild(divMensaje);
+
+	setTimeout(() => {
+		divMensaje.remove();
+	}, 3000);
 }
