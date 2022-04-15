937bfd96e38075f123cef656ed0a15d7a18087ed
Maxi Mongelos <maximongelos@gmail.com>
2022-04-14 23:25:35 -0300

    aplicando async-await

--- js/app.js
+++ js/app.js
@@ -24,14 +24,23 @@ const obtenerCriptomonedas = (criptomonedas) =>
 		resolve(criptomonedas);
 	});
 
-function consultarCriptomonedas() {
+async function consultarCriptomonedas() {
 	const url =
 		'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD';
 
-	fetch(url)
-		.then((respuesta) => respuesta.json())
-		.then((resultado) => obtenerCriptomonedas(resultado.Data))
-		.then((criptomonedas) => selectCriptomonedas(criptomonedas));
+	// 	fetch(url)
+	// 		.then((respuesta) => respuesta.json())
+	// 		.then((resultado) => obtenerCriptomonedas(resultado.Data))
+	// 		.then((criptomonedas) => selectCriptomonedas(criptomonedas));
+
+	try {
+		const respuesta = await fetch(url);
+		const resultado = await respuesta.json();
+		const cripto = await obtenerCriptomonedas(resultado.Data);
+		selectCriptomonedas(cripto);
+	} catch (error) {
+		console.error(error);
+	}
 }
 
 function selectCriptomonedas(criptomonedas) {
@@ -82,18 +91,26 @@ function mostrarAlerta(mensaje) {
 	}
 }
 
-function consultarAPI() {
+async function consultarAPI() {
 	const {moneda, criptomoneda} = objBusqueda;
 
 	const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`;
 
 	mostrarSpinner();
 
-	fetch(url)
-		.then((respuesta) => respuesta.json())
-		.then((cotizacion) =>
-			mostrarCotizacionHTML(cotizacion.DISPLAY[criptomoneda][moneda])
-		);
+	// fetch(url)
+	// 	.then((respuesta) => respuesta.json())
+	// 	.then((cotizacion) =>
+	// 		mostrarCotizacionHTML(cotizacion.DISPLAY[criptomoneda][moneda])
+	// 	);
+
+	try {
+		const respuesta = await fetch(url);
+		const resultado = await respuesta.json();
+		mostrarCotizacionHTML(resultado.DISPLAY[criptomoneda][moneda]);
+	} catch (error) {
+		console.error(error);
+	}
 }
 
 function mostrarCotizacionHTML(cotizacion) {
